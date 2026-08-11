68422c29320c2584fff40276587d29c7ad8548d9
林玮 (Jade Lin) <linw1995@icloud.com>
2026-08-11 13:48:18 +0800

    Pin Rust toolchain

--- nix/toolchain.nix
+++ nix/toolchain.nix
@@ -1,1 +1,18 @@
-{pkgs}: {}
+{pkgs}: let
+  inherit (pkgs.stdenv) isDarwin;
+  rustVersion = "1.97.0";
+  rustToolchain = pkgs.rust-bin.stable.${rustVersion}.default.override {
+    extensions = [
+      "clippy"
+      "llvm-tools-preview"
+      "rust-analyzer"
+      "rust-src"
+      "rustfmt"
+    ];
+    targets =
+      ["wasm32-unknown-unknown"]
+      ++ pkgs.lib.optionals isDarwin ["x86_64-apple-darwin"];
+  };
+in {
+  inherit rustToolchain rustVersion;
+}
